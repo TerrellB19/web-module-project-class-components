30efdadfadc7f113ef06ce6bdb924fbd4da0454a
Terrell Booker <TerrellBooker19@gmail.com>
2023-04-12 20:14:49 -0400

    initial commit

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,19 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>todos</h1>
+        <ul>
+          <li>Do the laundry</li>
+          <li>Wash the dishes</li>
+          <li>Vacuum carpet</li>
+        </ul>
+
+        <form>
+          <input type='text'/>
+          <button>Add</button>  
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
